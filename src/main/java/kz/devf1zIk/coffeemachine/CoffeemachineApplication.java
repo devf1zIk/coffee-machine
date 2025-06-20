ab8d8ecd5160bef413f8785ddd138a2e1c8c3eab
daniyardoskulov <daniyar.doskulov@narxoz.kz>
2025-06-20 19:35:50 +0500

    initial commit

---
+++ src/main/java/kz/devf1zIk/coffeemachine/CoffeemachineApplication.java
@@ -0,0 +1,13 @@
+package kz.devf1zIk.coffeemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeemachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeemachineApplication.class, args);
+	}
+
+}
